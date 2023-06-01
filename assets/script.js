efa18ef395f25e350549afc6a88c3c632a9c7b5d
matiasfarias152 <matiasfarias152@gmail.com>
2023-06-01 14:01:02 -0400

    render, botones y alerta
    se creo el render, y el listener para el boton de editar

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 var registrar = () =>{
+    let eContenedorTabla = document.getElementById("contenedorTabla");
     let eNombre = document.getElementById("nombre");
     let eApellido = document.getElementById("apellido");
     let nombre = eNombre.value;
     let apellido = eApellido.value;
-    console.log(nombre)
-    console.log(apellido)
+    console.log(nombre);
+    console.log(apellido);
     let persona = {"nombre":nombre,"apellido":apellido}
     let listadoPersonas = localStorage.getItem('personas');
     let listadoAntiguo = JSON.parse(listadoPersonas);
@@ -16,11 +17,32 @@ var registrar = () =>{
     }
     console.log(persona)
     console.log(listadoAntiguo)
-    console.log(listadoNuevo)
+    console.log(listadoNuevo);
     localStorage.setItem('personas',JSON.stringify(listadoNuevo));
+    //eContenedorTabla.innerHTML = ""+listadoNuevo.length;
+    render = "<table>"
+    render+= "<tr><th>Nombre</th><th>Apellido</th><th>Accion</th></tr>"
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element = listadoNuevo[i];
+        render+="<tr>"
+        render+="<td>"+element.nombre+"</td>"
+        render+="<td>"+element.apellido+"</td>"
+        render+="<td>"
+        render+="<button id='btnEditar"+i+"'>Editar</button>"
+        render+="<button>Eliminar</button>"
+        render+="</td>"
+        render+="</tr>"
+    }
+    render += "</table>";
+    eContenedorTabla.innerHTML = render;
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        var eBtn = document.getElementById("btnEditar"+i);
+        let element = listadoNuevo[i]
+        eBtn.addEventListener("click",()=>{alert("hola "+element.nombre+" "+element.apellido)})
+        
+    }
 }
 
-document.getElementById("btn").addEventListener("click",registrar)
+document.getElementById("btn").addEventListener("click",registrar);
 
-//prox clase guardar todo en el localStorage
-//cada vez que se presiona el registrar se agregue la lista, y si ya esta registrado que se siga agregando.  Para eliminar lograr mostrar todo lo que esta en la tabla, generar una tabla  y mostrar todo lo q esta guardado
+//Para eliminar lograr mostrar todo lo que esta en la tabla, generar una tabla  y mostrar todo lo q esta guardado, agregar los botones editar y eliminar
